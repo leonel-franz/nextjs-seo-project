9c91071da783dd0bc0b5e9e568dd78858c8b9970
leonel-franz <leonel.portillo@tecsup.edu.pe>
2026-06-18 12:12:05 -0500

    fix: urlset definitivo con namespace oficialwarwa

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,42 +1,41 @@
 export default async function handler(req, res) {
-  // Datos simulados de tu base de datos
   const postsDinamicos = [
     { slug: "introduccion-a-nextjs" },
     { slug: "guia-seo-avanzado" },
   ];
 
-  // Páginas fijas
   const paginasEstaticas = ["", "blog", "contacto"];
-  
-  // Tu subdominio de Render
+
   const dominioReal = "https://nextjs-seo-project.onrender.com";
 
-  // CORREGIDO: Formato urlset oficial y completo con comillas limpias
-  let xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-  xml += "<urlset xmlns=\"http://sitemaps.org\">";
+  let xml = '<?xml version="1.0" encoding="UTF-8"?>';
 
-  // Páginas estáticas
+  xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+
   paginasEstaticas.forEach((page) => {
-    const ruta = page === "" ? "" : "/" + page;
-    xml += '<url>';
-    xml += '<loc>' + dominioReal + ruta + '</loc>';
-    xml += '<changefreq>daily</changefreq>';
-    xml += '<priority>' + (page === "" ? "1.0" : "0.8") + '</priority>';
-    xml += '</url>';
+    const ruta = page ? `/${page}` : "";
+
+    xml += `
+      <url>
+        <loc>${dominioReal}${ruta}</loc>
+        <changefreq>daily</changefreq>
+        <priority>${page === "" ? "1.0" : "0.8"}</priority>
+      </url>
+    `;
   });
-  
-  // Rutas dinámicas
+
   postsDinamicos.forEach((post) => {
-    xml += '<url>';
-    xml += '<loc>' + dominioReal + '/blog/' + post.slug + '</loc>';
-    xml += '<changefreq>weekly</changefreq>';
-    xml += '<priority>0.6</priority>';
-    xml += '</url>';
+    xml += `
+      <url>
+        <loc>${dominioReal}/blog/${post.slug}</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.6</priority>
+      </url>
+    `;
   });
 
-  xml += '</urlset>';
+  xml += "</urlset>";
 
-  res.setHeader("Content-Type", "text/xml");
-  res.write(xml);
-  res.end();
+  res.setHeader("Content-Type", "application/xml");
+  res.status(200).send(xml);
 }
